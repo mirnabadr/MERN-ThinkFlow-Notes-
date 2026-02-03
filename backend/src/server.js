ad8b2c6c6e6bc922162574822cf4b14afc960ba3
Mirna badr <mirnabadr@Mirnas-MacBook-Air.local>
2026-02-03 19:21:35 +0200

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,31 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"; 
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config({ quiet: true });
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // CORS must run first so all responses include Access-Control-Allow-Origin
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 // middleware to parse the request body
 app.use(express.json());
 
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
